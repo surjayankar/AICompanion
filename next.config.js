7029b84ee0474d550c3b5638cc5d61b05dafbef2
Surjayan Kar <surjayan.smartboy@gmail.com>
2025-08-19 16:07:10 +0530

    Update before history rewrite

--- next.config.js
+++ next.config.js
@@ -29,6 +29,12 @@ const nextConfig = {
         port: "",
         pathname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "static.vecteezy.com", 
+        port: "",
+        pathname: "**",
+      },
     ],
   },
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar() {
                 height={0}
                 sizes="100vw"
                 className="block h-8 w-auto lg:hidden rounded-lg"
-                src="https://avatars.githubusercontent.com/u/745163?s=200&v=4"
+                src="https://static.vecteezy.com/system/resources/previews/008/165/790/large_2x/ai-logo-design-vector.jpg"
                 alt="a16z"
               />
               <Image
@@ -35,7 +35,7 @@ export default function Navbar() {
                 height={0}
                 sizes="100vw"
                 className="hidden h-8 w-auto lg:block rounded-lg"
-                src="https://avatars.githubusercontent.com/u/745163?s=200&v=4"
+                src="https://static.vecteezy.com/system/resources/previews/008/165/790/large_2x/ai-logo-design-vector.jpg"
                 alt="a16z"
               />
             </div>
@@ -56,16 +56,7 @@ export default function Navbar() {
                     {item.name}
                   </a>
                 ))}
-                <div className="px-3 py-2 text-gray-300">
-                  <iframe
-                    src="https://ghbtns.com/github-btn.html?user=a16z-infra&repo=companion-app&type=star&count=true"
-                    frameBorder="0"
-                    scrolling="0"
-                    width="150"
-                    height="20"
-                    title="GitHub"
-                  ></iframe>
-                </div>
+                
               </div>
             </div>
           </div>
